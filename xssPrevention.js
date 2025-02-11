4c3990cdf10ee1a55d4e9f771d3b834325ca16f3
thecoderspace <anitharaniganesh01@gmail.com>
2025-02-11 15:22:03 +0530

    Update xssPrevention.js

--- xssPrevention.js
+++ xssPrevention.js
@@ -1,7 +1,19 @@
 const XSSPrevention = {
-    sanitizeHTML: (input) => input.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#x27;"),
+    sanitizeHTML: (input) => input
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#x27;"),
     
-    sanitizeJavaScript: (input) => input.replace(/'/g, "\\'").replace(/"/g, '\\"').replace(//g, "\\"),
+    sanitizeJavaScript: (input) => input
+        .replace(/'/g, "\\'")
+        .replace(/"/g, '\\"')
+        .replace(/`/g, "\\`")
+        .replace(/\\/g, "\\\\")
+        .replace(/\$/g, "\\$")
+        .replace(/\(/g, "\\(")
+        .replace(/\)/g, "\\)"),
     
     sanitizeHTMLAttributes: (input) => input.replace(/"/g, "&quot;").replace(/'/g, "&#x27;"),
     
@@ -9,17 +21,46 @@ const XSSPrevention = {
     
     encodeURL: (input) => encodeURIComponent(input),
     
-    sanitizeASCII: (input) => input.replace(/&/g, "&#38;").replace(/</g, "&#60;").replace(/>/g, "&#62;").replace(/'/g, "&#39;").replace(/"/g, "&#34;"),
+    sanitizeASCII: (input) => input
+        .replace(/&/g, "&#38;")
+        .replace(/</g, "&#60;")
+        .replace(/>/g, "&#62;")
+        .replace(/'/g, "&#39;")
+        .replace(/"/g, "&#34;")
+        .replace(/`/g, "&#96;")
+        .replace(/\\/g, "&#92;"),
     
-    escapeJavaScriptString: (input) => input.replace(/\\/g, "\\\\").replace(/'/g, "\\'").replace(/"/g, "\\\"").replace(/\n/g, "\\n").replace(/\r/g, "\\r"),
+    escapeJavaScriptString: (input) => input
+        .replace(/\\/g, "\\\\")
+        .replace(/'/g, "\\'")
+        .replace(/"/g, "\\\"")
+        .replace(/\n/g, "\\n")
+        .replace(/\r/g, "\\r"),
     
-    escapeJavaString: (input) => input.replace(/\\/g, "\\\\").replace(/"/g, "\\\"").replace(/'/g, "\\'").replace(/\n/g, "\\n").replace(/\r/g, "\\r"),
+    escapeJavaString: (input) => input
+        .replace(/\\/g, "\\\\")
+        .replace(/"/g, "\\\"")
+        .replace(/'/g, "\\'")
+        .replace(/\n/g, "\\n")
+        .replace(/\r/g, "\\r"),
     
     removeScriptTags: (input) => input.replace(/<script.*?>.*?<\/script>/gi, ""),
     
-    advancedSanitize: (input) => input.replace(/<(script|iframe|object|embed|form).*?>.*?<\/\1>/gi, ""),
+    removeImageTags: (input) => input.replace(/<img.*?>/gi, ""),
     
-    escapeJSON: (input) => input.replace(/"/g, "\\\"").replace(/</g, "\\u003C").replace(/>/g, "\\u003E"),
+    removeSVGTags: (input) => input.replace(/<svg.*?>.*?<\/svg>/gi, ""),
+    
+    removeIframeTags: (input) => input.replace(/<iframe.*?>.*?<\/iframe>/gi, ""),
+    
+    removeMetaTags: (input) => input.replace(/<meta.*?>/gi, ""),
+    
+    advancedSanitize: (input) => input.replace(/<(script|iframe|object|embed|form|img|svg|meta|link|style).*?>.*?<\/\1>/gi, ""),
+    
+    escapeJSON: (input) => input
+        .replace(/"/g, "\\\"")
+        .replace(/</g, "\\u003C")
+        .replace(/>/g, "\\u003E")
+        .replace(/'/g, "\\u0027"),
     
     validateInput: (input) => /^[a-zA-Z0-9_ ]*$/.test(input) ? input : "Invalid input",
     
@@ -27,7 +68,25 @@ const XSSPrevention = {
     
     removeEventHandlers: (input) => input.replace(/on\w+=".*?"/gi, ""),
     
-    sanitizeCSS: (input) => input.replace(/expression\(|javascript:/gi, "")
+    sanitizeCSS: (input) => input.replace(/expression\(|javascript:/gi, ""),
+    
+    sanitizeAdvancedASCII: (input) => input.replace(/[\u0080-\uFFFF]/g, (char) => `&#${char.charCodeAt(0)};`),
+    
+    removeCommentTags: (input) => input.replace(/<!--.*?-->/g, ""),
+    
+    sanitizeJSONKeys: (input) => input.replace(/[^a-zA-Z0-9_]/g, ""),
+    
+    removeCDATASections: (input) => input.replace(/<!\[CDATA\[.*?\]\]>/gi, ""),
+    
+    removeStyleTags: (input) => input.replace(/<style.*?>.*?<\/style>/gi, ""),
+    
+    removeLinkTags: (input) => input.replace(/<link.*?>/gi, ""),
+    
+    removeNoscriptTags: (input) => input.replace(/<noscript.*?>.*?<\/noscript>/gi, ""),
+    
+    neutralizeBaseHref: (input) => input.replace(/<base.*?>/gi, ""),
+    
+    stripHexEncoding: (input) => input.replace(/%[0-9A-F]{2}/gi, ""),
 };
 
-export default XSSPrevention;      
+export default XSSPrevention;
